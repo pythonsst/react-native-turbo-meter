bc7015c13024457cc4a66c6375509b544730138c
shivtiwari <pythonsst@gmail.com>
2025-02-16 19:29:31 +0530

    feat: optimize animations and fix label calculation

--- src/index.tsx
+++ src/index.tsx
@@ -1,190 +1,101 @@
-/* eslint import/no-unresolved: [2, { ignore: ['react-native', 'react'] }] */
-/* eslint radix: ["error", "as-needed"] */
-
 import React, { useEffect, useRef } from 'react';
-import {
-  View,
-  Image,
-  Animated,
-  Easing,
-  Text,
-  Dimensions,
-} from 'react-native';
+import { View, Image, Animated, Easing, Text, StyleSheet, Dimensions } from 'react-native';
 
-// Utils
-import { calculateDegreeFromLabels } from './utils/calculate-degree-from-labels';
-import { calculateLabelFromValue } from './utils/calculate-label-from-value';
-import { limitValue } from './utils/limit-value';
-import { validateSize } from './utils/validate-size';
-
-// Style
-import styles from './style';
+// Import utility functions
+import {calculateDegreeFromLabels} from './utils/calculate-degree-from-labels';
+import {calculateLabelFromValue} from './utils/calculate-label-from-value';
+import {limitValue} from './utils/limit-value';
+import {validateSize} from './utils/validate-size';
 
 const deviceWidth = Dimensions.get('window').width;
 
-type SpeedometerLabel = {
+type Label = {
   name: string;
   labelColor: string;
   activeBarColor: string;
 };
 
-interface SpeedometerProps {
+type SpeedometerProps = {
   value: number;
-  defaultValue?: number;
   size?: number;
   minValue?: number;
   maxValue?: number;
+  labels?: Label[];
   easeDuration?: number;
-  allowedDecimals?: number;
-  labels?: SpeedometerLabel[];
-  needleImage: any;
-  wrapperStyle?: object;
-  outerCircleStyle?: object;
-  halfCircleStyle?: object;
-  imageWrapperStyle?: object;
-  imageStyle?: object;
-  innerCircleStyle?: object;
-  labelWrapperStyle?: object;
-  labelStyle?: object;
-  labelNoteStyle?: object;
+  needleImage?: any;
   useNativeDriver?: boolean;
-}
+};
 
-const defaultLabels: SpeedometerLabel[] = [
-  { name: 'Weak', labelColor: '#ff0000', activeBarColor: '#ff0000' },
-  { name: 'Strong', labelColor: '#00ff00', activeBarColor: '#00ff00' },
+const defaultLabels: Label[] = [
+  { name: 'Too Slow', labelColor: '#ff2900', activeBarColor: '#ff2900' },
+  { name: 'Very Slow', labelColor: '#ff5400', activeBarColor: '#ff5400' },
+  { name: 'Slow', labelColor: '#f4ab44', activeBarColor: '#f4ab44' },
+  { name: 'Normal', labelColor: '#f2cf1f', activeBarColor: '#f2cf1f' },
+  { name: 'Fast', labelColor: '#14eb6e', activeBarColor: '#14eb6e' },
+  { name: 'Unbelievably Fast', labelColor: '#00ff6b', activeBarColor: '#00ff6b' },
 ];
 
 const Speedometer: React.FC<SpeedometerProps> = ({
   value,
-  defaultValue = 50,
-  size,
+  size = deviceWidth - 20,
   minValue = 0,
   maxValue = 100,
-  easeDuration = 500,
-  allowedDecimals = 0,
   labels = defaultLabels,
-  needleImage,
-  wrapperStyle,
-  outerCircleStyle,
-  halfCircleStyle,
-  imageWrapperStyle,
-  imageStyle,
-  innerCircleStyle,
-  labelWrapperStyle,
-  labelStyle,
-  labelNoteStyle,
+  easeDuration = 500,
+  needleImage = require('./images/speedometer-needle.png'),
   useNativeDriver = true,
 }) => {
-  const speedometerValue = useRef(new Animated.Value(defaultValue)).current;
+  const limitedValue = limitValue(value, minValue, maxValue, 0);
+  const validatedSize = validateSize(size, deviceWidth - 20);
+  const perLevelDegree = calculateDegreeFromLabels(180, labels);
+  const label = calculateLabelFromValue(limitedValue, labels, minValue, maxValue);
+
+  const rotation = useRef(new Animated.Value((limitedValue / maxValue) * 180 - 90)).current;
 
   useEffect(() => {
-    Animated.timing(speedometerValue, {
-      toValue: limitValue(value, minValue, maxValue, allowedDecimals),
+    Animated.timing(rotation, {
+      toValue: (limitedValue / maxValue) * 180 - 90,
       duration: easeDuration,
       easing: Easing.linear,
       useNativeDriver,
     }).start();
-  }, [value, minValue, maxValue, allowedDecimals, easeDuration, useNativeDriver]);
-
-  const degree = 180;
-  const perLevelDegree = calculateDegreeFromLabels(degree, labels);
-  const limitedValue = limitValue(value, minValue, maxValue, allowedDecimals);
-  const label = calculateLabelFromValue(limitedValue, labels, minValue, maxValue);
-
-  const rotate = speedometerValue.interpolate({
-    inputRange: [minValue, maxValue],
-    outputRange: ['-90deg', '90deg'],
-  });
-
-  const currentSize = validateSize(size, deviceWidth - 20);
+  }, [limitedValue, maxValue, easeDuration, useNativeDriver]);
 
   return (
-    <View
-      style={[
-        styles.wrapper,
-        { width: currentSize, height: currentSize / 2 },
-        wrapperStyle,
-      ]}
-    >
-      <View
+    <View style={[styles.container, { width: validatedSize, height: validatedSize / 2 }]}>
+      <Animated.Image
+        source={needleImage}
         style={[
-          styles.outerCircle,
+          styles.needle,
           {
-            width: currentSize,
-            height: currentSize / 2,
-            borderTopLeftRadius: currentSize / 2,
-            borderTopRightRadius: currentSize / 2,
+            width: validatedSize * 0.1,
+            height: validatedSize * 0.4,
+            top: validatedSize * 0.1,
+            left: validatedSize * 0.45,
+            transform: [{ rotate: rotation.interpolate({ inputRange: [-90, 90], outputRange: ['-90deg', '90deg'] }) }],
           },
-          outerCircleStyle,
         ]}
-      >
-        {labels.map((level, index) => {
-          const circleDegree = 90 + index * perLevelDegree;
-          return (
-            <View
-              key={level.name}
-              style={[
-                styles.halfCircle,
-                {
-                  backgroundColor: level.activeBarColor,
-                  width: currentSize / 2,
-                  height: currentSize,
-                  borderRadius: currentSize / 2,
-                  transform: [
-                    { translateX: currentSize / 4 },
-                    { rotate: `${circleDegree}deg` },
-                    { translateX: -currentSize / 4 },
-                  ],
-                },
-                halfCircleStyle,
-              ]}
-            />
-          );
-        })}
-        <Animated.View
-          style={[
-            styles.imageWrapper,
-            { top: -(currentSize / 15), transform: [{ rotate }] },
-            imageWrapperStyle,
-          ]}
-        >
-          <Image
-            style={[
-              styles.image,
-              { width: currentSize, height: currentSize },
-              imageStyle,
-            ]}
-            source={needleImage}
-          />
-        </Animated.View>
-        <View
-          style={[
-            styles.innerCircle,
-            {
-              width: currentSize * 0.6,
-              height: (currentSize / 2) * 0.6,
-              borderTopLeftRadius: currentSize / 2,
-              borderTopRightRadius: currentSize / 2,
-            },
-            innerCircleStyle,
-          ]}
-        />
-      </View>
-      <View style={[styles.labelWrapper, labelWrapperStyle]}>
-        <Text style={[styles.label, labelStyle]}>{limitedValue}</Text>
-        <Text
-          style={[
-            styles.labelNote,
-            { color: label.labelColor },
-            labelNoteStyle,
-          ]}
-        >
-          {label.name}
-        </Text>
-      </View>
+        resizeMode="contain"
+      />
+      <Text style={[styles.label, { color: label.labelColor }]}>{label.name}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+  },
+  needle: {
+    position: 'absolute',
+  },
+  label: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default Speedometer;
